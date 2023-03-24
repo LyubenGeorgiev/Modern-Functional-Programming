b3f6209e90891423c4070c7153e23c3563e881ef
LyubenGeorgiev <79706202+LyubenGeorgiev@users.noreply.github.com>
2023-03-24 23:10:48 +0200

    little changes

--- week01.hs
+++ week01.hs
@@ -2,23 +2,25 @@
 -- Remove duplicates from a list and maintain original order of elements
 setof :: (Eq a) => [a] -> [a]
 setof [] = []
-setof (x:xs) = x : (setof $ filter (/=x) xs)
+setof (x:xs) = x : setof (filter (/=x) xs)
  
 -- Task 2
 -- Given an element X and list of elements return list of 
 -- lists with elements representing every possible insertion of the element X
 insertat :: a -> [a] -> [[a]]
 insertat x [] = [[x]]
-insertat x (y:ys) = (x:y:rest) : (map (y:) whole)
+insertat x (y:ys) = (x:y:rest) : map (y:) whole
     where whole@((_:rest):_) = insertat x ys
  
 -- Task 3
 -- Given a list return list of lists representing 
 -- every permutation of the elements in the original list
 perms :: [a] -> [[a]]
-perms [] = []
-perms [a] = [[a]]
+perms [] = [[]]
 perms (x:xs) = concat $ map (insertat x) $ perms xs
+-- Another way is:
+-- perms [] = [[]]
+-- perms (x:xs) = [zs | ys <- perms xs, zs <- insertat x ys]
  
 -- Task 4
 -- Given a list return list of lists representing every subset of the original list
